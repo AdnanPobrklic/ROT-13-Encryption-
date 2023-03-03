073840275a46a52ec71565877ac79c7aa3e83956
Adnan <adnanppobrklic@gmail.com>
2023-03-03 22:17:07 +0100

    fixed numbers and symbols not rendering

--- script.js
+++ script.js
@@ -1,54 +1,35 @@
-const btn = document.querySelector('button')
-const pTag = document.querySelector('p')
-const textArea = document.querySelector('textarea')
-const tempInput = document.querySelector('#temp-input')
-const lowerAlphabet = [];
-const upperAlphabet = [];
-let inputLetters = [];
+const btn = document.querySelector('button');
+const pTag = document.querySelector('p');
+const textArea = document.querySelector('textarea');
+const tempInput = document.querySelector('#temp-input');
+const lowerAlphabet = [...Array(26)].map((_, i) => String.fromCharCode(97 + i));
+const upperAlphabet = [...Array(26)].map((_, i) => String.fromCharCode(65 + i));
 
-for(let i = 0; i < 26; i++){
-    let letter = String.fromCharCode(65+i)
-    upperAlphabet.push(letter)
-}
-
-for(let i = 0; i < 26; i++){
-    let letter = String.fromCharCode(97+i)
-    lowerAlphabet.push(letter)
+function conversion(iterator, outputArray, firstArray, secondArray) {
+  if (/[a-zA-Z]/.test(iterator)) {
+    const alphabet = firstArray.indexOf(iterator) > -1 ? firstArray : secondArray;
+    let letterPosition = alphabet.indexOf(iterator) + 13;
+    if (letterPosition > 25) {
+      letterPosition %= 26;
+    }
+    outputArray.push(alphabet[letterPosition]);
+  } else {
+    outputArray.push(iterator);
+  }
 }
 
 btn.addEventListener('click', () => {
-    let outputArr = []
-    inputLetters = textArea.value
-    textArea.value = "";
-    inputLetters = inputLetters.split('')
-    inputLetters.forEach(i => {
-       conversion(i,outputArr,upperAlphabet,lowerAlphabet)
-    })
-    pTag.innerHTML =` ${outputArr.join('')}  <button id="copyBtn" onclick="copy()"><i class="fa-solid fa-copy"></i></button> `
-    tempInput.value = outputArr.join('')
-})
+  const outputArr = [];
+  const inputLetters = textArea.value.split('');
+  inputLetters.forEach(i => conversion(i, outputArr, upperAlphabet, lowerAlphabet));
+  textArea.value = '';
+  pTag.innerHTML = `${outputArr.join('')}<button id="copyBtn" onclick="copy()"><i class="fa-solid fa-copy"></i></button>`;
+  tempInput.value = outputArr.join('');
+});
 
-function conversion(iterator,outputArray,firstArray,secondArray) {
-    if(firstArray.indexOf(iterator) > -1) {
-        let letterPosition = firstArray.indexOf(iterator) + 13
-        if(letterPosition > 25){
-            letterPosition %= 26
-        }
-        outputArray.push((firstArray[letterPosition]))
-    }else if(secondArray.indexOf(iterator) > -1){
-        let letterPosition = secondArray.indexOf(iterator) + 13
-        if(letterPosition > 25){
-            letterPosition %= 26
-        }
-        outputArray.push((secondArray[letterPosition]))
-    }else{
-        outputArray.push(' ')
-    }
-}
-
-function copy(){
-    tempInput.select();
-    tempInput.setSelectionRange(0, 99999)
-    navigator.clipboard.writeText(tempInput.value);
-    alert('Copied to clipboard')
+function copy() {
+  tempInput.select();
+  tempInput.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(tempInput.value);
+  alert('Copied to clipboard');
 }
